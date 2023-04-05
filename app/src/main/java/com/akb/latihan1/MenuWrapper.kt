c8e7e15791ef56dbfe7c55125c2483849bf1d6e7
Rizka Ghinna Auliya <rizkaghina29@gmail.com>
2023-04-06 00:55:27 +0700

    Latihan 1 - 10120908 - Rizka Ghinna Auliya

---
+++ app/src/main/java/com/akb/latihan1/MenuWrapper.kt
@@ -0,0 +1,13 @@
+package com.akb.latihan1
+
+/**
+ * Tanggal Pengerjaan : 06/04/2023
+ * NIM : 10120908
+ * Nama : Rizka Ghinna Auliya
+ * Kelas : IF-10
+ * */
+
+data class MenuWrapper(
+    val image: Int,
+    val title: Int
+)
